376bebd1e689e468770cc6255c18d4408f686348
khalil nouisser <knouisser@zaion.ai>
2022-08-12 18:29:36 +0100

    V1

--- src/app/layouts/auth-layout/auth-layout.module.ts
+++ src/app/layouts/auth-layout/auth-layout.module.ts
@@ -4,7 +4,7 @@ import {CommonModule} from '@angular/common';
 import {ReactiveFormsModule} from '@angular/forms';
 
 import {LoginComponent} from '../../pages/login/login.component';
-$
+
 @NgModule({
   imports: [
     CommonModule,
