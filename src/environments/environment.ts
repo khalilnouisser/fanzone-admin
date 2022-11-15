b3d8ab913393d34d630df02eab6cc9eee5d2a24e
khalilnouisser <med.khalilnouisser@gmail.com>
2022-11-15 19:32:03 +0000

    changes url

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  serverUrl: 'http://http://35.241.174.61:3000',
-  uploadImageServer: 'http://http://35.241.174.61:3000',
+  serverUrl: 'http://34.77.112.234:3000',
+  uploadImageServer: 'http://34.77.112.234:3000',
 };
